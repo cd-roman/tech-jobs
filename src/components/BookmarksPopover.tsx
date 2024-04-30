847281106a96d76d7efd25882b1d96806a32251b
Roman <60399380+cd-roman@users.noreply.github.com>
2024-04-30 04:28:06 +0200

    Update BookmarksPopover component to display empty message when there are no bookmarks

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -6,12 +6,33 @@ import { createPortal } from "react-dom";
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
+  const content =
+    !isLoading && bookmarkedJobItems.length === 0 ? (
+      <EmptyBookmarksPopover />
+    ) : (
+      <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
+    );
+
   return createPortal(
     <div ref={ref} className="bookmarks-popover">
-      <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
+      {content}
     </div>,
     document.body
   );
 });
+
+function EmptyBookmarksPopover() {
+  return (
+    <div className="bookmarks-popover-empty-message">
+      <p className="bookmarks-popover-heading">
+        You don't have any bookmarks yet
+      </p>
+      <p className="bookmarks-popover-subheading">
+        Add bookmarks by clicking the bookmark icon next to any job in the job
+        list from your search results
+      </p>
+    </div>
+  );
+}
 
 export default BookmarksPopover;
