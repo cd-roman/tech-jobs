0f0791d56b1be60a0e6054965ca89973d5f5716b
Roman <60399380+cd-roman@users.noreply.github.com>
2024-04-30 02:10:15 +0200

    Update favicon and bg pattern, fix form input notification

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,10 +1,7 @@
 export default function Background() {
   return (
     <div className="background">
-      <img
-        src="https://bytegrad.com/course-assets/js/2/pattern.svg"
-        alt="Background pattern"
-      />
+      <img src="/assets/pattern.png" alt="Background pattern" />
     </div>
   );
 }
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -16,6 +16,7 @@ export default function SearchForm() {
       </button>
 
       <input
+        name="search"
         value={searchText}
         onChange={(e) => {
           handleChangeSearchText(e.target.value);
